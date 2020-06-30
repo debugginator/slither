e2b57fb661b82970fc5464133817eff919588f68
debugginator <blaz.bagic@taxfix.de>
2020-06-30 12:30:11 +0200

    Update front running transitive test suite

--- tests/front-running/front-running-transitive.sol
+++ tests/front-running/front-running-transitive.sol
@@ -7,16 +7,12 @@ contract Test {
     constructor() public {
     }
 
-    function unsafeWithdraw(uint amount) public returns(bool){
-        return _unsafeWithdraw(amount);
+    function unsafeWithdraw(uint amount) public {
+        _unsafeWithdraw(amount);
     }
 
-    function _unsafeWithdraw(uint amount) private returns(bool){
-        if (msg.sender.send(amount)) {
-            return true;
-        } else {
-            revert();
-        }
+    function _unsafeWithdraw(uint amount) private {
+        msg.sender.transfer(amount);
     }
 
     function unsafeWithdraw2(uint amount) public{
@@ -24,30 +20,10 @@ contract Test {
     }
 
     function _unsafeWithdraw2(uint amount) private{
+        __unsafeWithdraw2(amount);
+    }
+
+    function __unsafeWithdraw2(uint amount) internal{
         msg.sender.transfer(amount);
     }
-
-    // below are legitimate calls
-    // and should not be detected
-    function commit(bytes32 hash) payable public {
-        hashedData = hash;
-    }
-
-    function reveal(uint data, uint amount) public returns(bool){
-        return _reveal(data, amount);
-    }
-
-    function _reveal(uint data, uint amount) public returns(bool){
-        require(keccak256(abi.encodePacked(data)) == hashedData);
-        if (msg.sender.send(amount)) {
-            return true;
-        } else {
-            revert();
-        }
-    }
-
-    function sendToPredefinedAddress(uint value) public{
-        address(0x9446980D667FE43Cc537c120Fb16718771f3d882).transfer(value);
-    }
-
 }
