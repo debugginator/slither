dbca6f4af4be9126356e3f70f52fdb60af423b97
debugginator <blaz.bagic@taxfix.de>
2020-06-30 12:30:33 +0200

    Update front running unrelated state test suite

--- tests/front-running/front-running-unsafe-global-access.sol
+++ tests/front-running/front-running-unsafe-global-access.sol
@@ -1,53 +1,24 @@
 pragma solidity ^0.5.14;
 
-contract Test {
+contract UnrelatedStateTest {
 
-    bytes32 hashedData;
+    address public owner;
+    uint8 public MAX_WITHDRAW_AMOUNT = 10;
+    mapping (address => uint256) public balanceOf;
 
-    constructor() public {
+    constructor(address _owner) public {
+        owner = _owner;
     }
 
-    function unsafeWithdraw(uint amount) public returns(bool){
-        return _unsafeWithdraw(amount);
-    }
-
-    function _unsafeWithdraw(uint amount) public returns(bool){
-        if (msg.sender.send(amount)) {
-            return true;
-        } else {
-            revert();
-        }
-    }
-
-    function unsafeWithdraw2(uint amount) public{
-        return _unsafeWithdraw2(amount);
-    }
-
-    function _unsafeWithdraw2(uint amount) public{
+    function unsafeWithdraw(uint amount) public{
+        require(amount < MAX_WITHDRAW_AMOUNT);
         msg.sender.transfer(amount);
     }
 
-    // below are legitimate calls
-    // and should not be detected
-    function commit(bytes32 hash) payable public {
-        hashedData = hash;
-    }
+    function safeWithdraw(uint amount) public{
+        require(balanceOf[msg.sender] > amount);
 
-    function reveal(uint data, uint amount) public returns(bool){
-        return _reveal(data, amount);
+        balanceOf[msg.sender] = balanceOf[msg.sender] - amount;
+        msg.sender.transfer(amount);
     }
-
-    function _reveal(uint data, uint amount) public returns(bool){
-        require(keccak256(abi.encodePacked(data)) == hashedData);
-        if (msg.sender.send(amount)) {
-            return true;
-        } else {
-            revert();
-        }
-    }
-
-    function sendToPredefinedAddress(uint value) public{
-        address(0x9446980D667FE43Cc537c120Fb16718771f3d882).transfer(value);
-    }
-
 }
